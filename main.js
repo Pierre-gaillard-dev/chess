e42302146068b8386a396647fe8555453e4a45e3
Pierre-gaillard-dev <pierre.gaillard.dev@gmail.com>
2024-08-14 09:43:41 +0000

    V0.0.1

--- main.js
+++ main.js
@@ -11,19 +11,28 @@ function init() {
 
     table.innerHTML = ""
 
-    for (let row = 8; row >= 0; row++) {
+    for (let row = 8; row > 0; row--) {
         let new_row = document.createElement("tr");
         new_row.classList = "table_row";
         new_row.id = `tr-${row}`;
 
         table.appendChild(new_row)
 
-        for (let square=0; square <= 8; square++) {
-            let new_square = document.createElement("td");
-            new_square.classList = "table_square"
-            new_square.id = `td_${row}-${numberToLetter(square)}`
+        for (let tile=1; tile <= 8; tile++) {
+            let new_tile = document.createElement("td");
+            new_tile.classList = "table_tile"
+            new_tile.id = `td_${row}-${numberToLetter(tile)}`
+
+            new_tile.setAttribute("row", row);
+            new_tile.setAttribute("column", tile);
+
+            if ((row + tile)%2 == 1) {
+                new_tile.classList += " white"
+            } else {
+                new_tile.classList += " black"
+            }
             
-            new_row.appendChild(new_square)
+            new_row.appendChild(new_tile)
         }
     }
 }
